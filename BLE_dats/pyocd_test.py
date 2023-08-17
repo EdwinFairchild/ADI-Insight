1a6c9c862fe61c9124d82c9e5b7dcd19cbf28c7b
Eddie <eddie.amaya@analog.com>
2023-08-17 14:24:40 -0500

    testing mass erase, NOT working

--- BLE_dats/pyocd_test.py
+++ BLE_dats/pyocd_test.py
@@ -2,10 +2,40 @@ from pyocd.core.helpers import ConnectHelper
 #from target_max32655 import MAX32655  # Make sure this import corresponds to the actual location of your custom target file
 import time
 import logging
+import subprocess
 logging.basicConfig(level=logging.DEBUG)
+
+def mass_erase():
+    command = ["pyocd", "erase", "--mass"]
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print("Mass erase failed:", result.stderr.decode())
+    else:
+        print("Mass erase completed.")
+
+
+
+def print_core_registers(target):
+    # Define the list of core registers you want to read
+    reg_list = ['r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'r10', 'r11', 'r12', 'sp', 'lr', 'pc', 'xpsr']
+
+    #halt tagert
+    target.halt()
+    # Get the core registers
+    core_registers = target.read_core_registers_raw(reg_list)
+    #resume target
+    target.resume()
+    
+    # Print the core registers
+    print("Core Registers:")
+    for reg, value in zip(reg_list, core_registers):
+        print(f"{reg}: {value:08x}")
+
+
 
 def monitor_variables(target, addresses):
     previous_value = None
+    currentTime = time.time()
     while True:
         for address in addresses:
             value = target.read32(address)
@@ -13,6 +43,13 @@ def monitor_variables(target, addresses):
                 previous_value = value
                 print(f"Variable at {address:x}: {value}")
         time.sleep(0.1)  # Adjust the refresh rate as needed
+        # after 10 seconds do a mass erase
+        if time.time() - currentTime > 5:
+            target.halt()
+            target.disconnect()  # Disconnect before erase
+            mass_erase()
+            print("Mass erase completed.")
+            break;
 
 def main():
     session_options = {
@@ -32,11 +69,12 @@ def main():
         target.resume() # Add this line
        #variable_addresses = [0x20001374, 0x20000008]  # Replace with actual addresses
        # address of counter varaible in wsf timer
-       
+        # Call the method to print core registers
+        print_core_registers(target)
         variable_addresses = [0x20001374]  # Replace with actual addresses
         monitor_variables(target, variable_addresses)
-
-        print("Connected to target, CPU ID:", target.core.cpu_id)
+        print("All done!")
+       # print("Connected to target, CPU ID:", target.cores.cpu_id)
 
 if __name__ == "__main__":
     main()
